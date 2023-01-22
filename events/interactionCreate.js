258d584aef64b29a3f21a43976b17ef7f66d3819
Kathy <65121394+kfahn22@users.noreply.github.com>
2023-01-22 17:39:03 -0500

    adding script

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,23 +1,47 @@
-const { Events } = require('discord.js');
-const fs = require('node:fs');
+// https://stackoverflow.com/questions/73028854/discord-js-v13-code-breaks-when-upgrading-to-v14
+// https://discord.js.org/#/docs/main/main/class/BaseInteraction?scrollTo=isStringSelectMenu
+
+const {
+	Events,
+	Component
+} = require('discord.js');
+const wait = require('node:timers/promises').setTimeout;
+
+// module.exports = {
+// 	name: Events.InteractionCreate,
+// 	async execute(interaction) {
+// 		if (!interaction.isChatInputCommand()) return;
+// 		const command = interaction.client.commands.get(interaction.commandName);
+
+// 		try {
+// 			await command.execute(interaction);
+// 		} catch (error) {
+// 			console.error(`Error executing ${interaction.commandName}`);
+// 			console.error(error);
+// 		}
+
+
+// 		if (interaction.isStringSelectMenu()) {
+// 			console.log('Hi Kathy')
+// 			await interaction.deferUpdate();
+// 			await wait(4000);
+// 			await interaction.update({
+// 				content: 'Something was selected!'
+// 			});
+// 		}
+// 	}
+// };
 
 module.exports = {
 	name: Events.InteractionCreate,
 	async execute(interaction) {
-		if (!interaction.isChatInputCommand()) return;
+		if (!interaction.isStringSelectMenu()) return;
 
-		const command = interaction.client.commands.get(interaction.commandName);
-
-		if (!command) {
-			console.error(`No command matching ${interaction.commandName} was found.`);
-			return;
+		if (interaction.customId === 'select-challenge') {
+			await interaction.update({
+				content: 'Something was selected!',
+				components: []
+			});
 		}
-
-		try {
-			await command.execute(interaction);
-		} catch (error) {
-			console.error(`Error executing ${interaction.commandName}`);
-			console.error(error);
-		}
-	},
+	}
 };
