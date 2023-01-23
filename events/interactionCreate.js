c81ac2d154f9a5bdc93222da642531db8c18e755
Kathy <65121394+kfahn22@users.noreply.github.com>
2023-01-22 20:19:57 -0500

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,46 +2,28 @@
 // https://discord.js.org/#/docs/main/main/class/BaseInteraction?scrollTo=isStringSelectMenu
 
 const {
-	Events,
-	Component
+	Events
 } = require('discord.js');
 const wait = require('node:timers/promises').setTimeout;
-
-// module.exports = {
-// 	name: Events.InteractionCreate,
-// 	async execute(interaction) {
-// 		if (!interaction.isChatInputCommand()) return;
-// 		const command = interaction.client.commands.get(interaction.commandName);
-
-// 		try {
-// 			await command.execute(interaction);
-// 		} catch (error) {
-// 			console.error(`Error executing ${interaction.commandName}`);
-// 			console.error(error);
-// 		}
-
-
-// 		if (interaction.isStringSelectMenu()) {
-// 			console.log('Hi Kathy')
-// 			await interaction.deferUpdate();
-// 			await wait(4000);
-// 			await interaction.update({
-// 				content: 'Something was selected!'
-// 			});
-// 		}
-// 	}
-// };
 
 module.exports = {
 	name: Events.InteractionCreate,
 	async execute(interaction) {
-		if (!interaction.isStringSelectMenu()) return;
+		if (interaction.isChatInputCommand()) {
+			const command = interaction.client.commands.get(interaction.commandName);
 
+			try {
+				await command.execute(interaction);
+			} catch (error) {
+				console.error(`Error executing ${interaction.commandName}`);
+				console.error(error);
+			}
+		}
+	
 		if (interaction.customId === 'select-challenge') {
-			await interaction.update({
-				content: 'Something was selected!',
-				components: []
-			});
+			const selected = interaction.values.join(', ');
+
+			await interaction.update(`The user selected ${selected}!`);
 		}
 	}
 };
