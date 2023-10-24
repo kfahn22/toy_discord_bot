41435ebea643d3d3da94bbb02b6418081533544e
Kathy <65121394+kfahn22@users.noreply.github.com>
2023-10-24 13:44:55 -0400

    update jokes

--- commands/jokes.js
+++ commands/jokes.js
@@ -24,43 +24,42 @@ export async function execute(interaction) {
       .setPlaceholder("Can you answer the joke?")
       .setMinValues(1)
       .setMaxValues(1)
-      .addOptions(
-        new StringSelectMenuOptionBuilder()
-          .setLabel(`jokes[${i}]`)
-          .setDescription(`Joke ${i}`)
-          .setValue(
-            `jokes[${i}].punchline.some(answer => answer.toLowerCase() === response.content.toLowerCase())`
-          )
-      )
+    // .addOptions(
+    //   new StringSelectMenuOptionBuilder()
+    //     .setLabel(`jokes[${i}]`)
+    //     .setDescription(`Joke ${i}`)
+    //     .setValue(
+    //       `jokes[${i}].punchline.some(answer => answer.toLowerCase() === response.content.toLowerCase())`
+    //     )
+    // )
   );
 
   const filter = (response) => {
-    return jokes[`${i}`].punchline.some(
+    return jokes[i].punchline.some(
       (answer) => answer.toLowerCase() === response.content.toLowerCase()
     );
   };
 
-  const answer = jokes[`${i}`].punchline;
-  await interaction
-    .reply({
-      content: jokes[`${i}`].joke,
-      fetchReply: true,
+  const answer = jokes[i].punchline;
+  await interaction.reply({
+    content: jokes[i].joke,
+    fetchReply: true,
+  });
+  const collected = await interaction.channel
+    .awaitMessages({
+      filter,
+      max: 1,
+      time: 20000, // Allow user response for 20 seconds
+      errors: ["time"],
     })
-    .then(() => {
-      interaction.channel
-        .awaitMessages({
-          filter,
-          max: 1,
-          time: 30000, // Allow user response for 30 seconds
-          errors: ["time"],
-        })
-        .then((collected) => {
-          interaction.followUp(`${collected.first().author} you got it right!`);
-        })
-        .catch((collected) => {
-          interaction.followUp(
-            `Sorry, that is't right.  The answer is ${answer}`
-          );
-        });
-    });
+    .catch(() =>
+      interaction.followUp(
+        `Sorry, that isn't right.  The punchline is ${answer}`
+      )
+    );
+  // If nothing got collected so it probably timed out and got handled by the catch above, do nothing...
+  if (!collected) return;
+
+  // They got it right!
+  await interaction.followUp(`${collected.first().author} you got it right!`);
 }
